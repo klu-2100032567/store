edd4132d3a27d8810b4a6f47e840523cf19f8257
klu-2100032567 <2100032567@kluniversity.in>
2025-03-17 13:46:51 +0530

    Added backend

--- src/index.js
+++ src/index.js
@@ -10,6 +10,20 @@ root.render(
     <App />
   </React.StrictMode>
 );
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
